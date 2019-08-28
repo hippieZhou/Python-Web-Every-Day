b0222bab88823e9af0e40e77052877028c19821f
hippieZhou <hippiezhou@outlook.com>
2019-08-28 22:45:02 +0800

    add djangoer

---
+++ 10 - djangoer/mysite/polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+# https://docs.djangoproject.com/zh-hans/2.2/intro/tutorial03/
